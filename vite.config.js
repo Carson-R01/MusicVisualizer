9b47a9afd8f5bc1852e1490af1f7639e8bda5492
carsonreiter <creiter3@my.wctc.edu>
2026-06-09 12:58:32 -0500

    Configure GitHub Pages branch deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/MusicVisualizer/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   plugins: [react()],
 });
